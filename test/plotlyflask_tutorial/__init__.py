914acedeb9f7ae5182918df899c2d340165eae65
Benedict Then <jxthen.2017@sis.smu.edu.sg>
2020-08-30 09:43:24 +0800

    updates

---
+++ test/plotlyflask_tutorial/__init__.py
@@ -0,0 +1,20 @@
+"""Initialize Flask app."""
+from flask import Flask
+
+
+def init_app():
+    """Construct core Flask application with embedded Dash app."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+
+    with app.app_context():
+        # Import parts of our core Flask app
+        from . import routes
+
+        # Import Dash application
+        from dashboard import init_dashboard
+        from dashboard2 import init_dashboard2
+        app = init_dashboard(app)
+        app = init_dashboard2(app)
+
+        return app
