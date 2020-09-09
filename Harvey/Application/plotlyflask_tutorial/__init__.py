006397e0b6658b779ec40677102542f17962755d
Harvey <43161937+harveylauw2017@users.noreply.github.com>
2020-09-09 15:44:48 +0800

    Deployment Week 4

--- Harvey/Application/plotlyflask_tutorial/__init__.py
+++ Harvey/Application/plotlyflask_tutorial/__init__.py
@@ -1,12 +1,10 @@
 """Initialize Flask app."""
 from flask import Flask
-import config
-
 
 def init_app():
     """Construct core Flask application with embedded Dash app."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+    app.config.from_object(app.config)
 
     with app.app_context():
         # Import parts of our core Flask app
