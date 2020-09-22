e26ccf704bb30603df046b113e166aed4aacdd9d
May Myo Myint Myat <mmmmay.2017@sis.smu.edu.sg>
2020-09-22 18:45:49 +0800

    Added Cash v Liab table and barchart

--- May/dash_pages/client_summary_pg.py
+++ May/dash_pages/client_summary_pg.py
@@ -3,10 +3,12 @@ from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+
+import plotly.express as px
+import plotly.graph_objects as go
+
 import pandas as pd
 import numpy as np
-import plotly.express as px
-
 import pandas_datareader.data as web
 import datetime
 
@@ -45,6 +47,8 @@ app.layout = html.Div(children=[
     dcc.Graph(id='asset_class_barchart'),
     dcc.Graph(id='asset_class_piechart'),
     dcc.Graph(id='asset_class_sunburst'),
+    dcc.Graph(id='cash_loans_table'),
+    dcc.Graph(id='cash_loans_barchart'),
     ])
 
 @app.callback(
@@ -122,6 +126,66 @@ def asset_class_sunburst(selected_base_numbers):
     title="Client's Asset Class-Sub Asset Class Breakdown")
     fig.update_traces(textinfo='percent parent+label')
     #fig.update_layout(margin=dict(t=0, b=0, l=0, r=0))
+    return fig
+
+@app.callback(
+    Output('cash_loans_table','figure'),
+    [Input('base_numbers_checklist', 'value')]
+) 
+def cash_loans_table(selected_base_numbers):
+    asset_classes = ['Investment Cash & Short Term Investments', 'Loans']
+    client_data = df[df["Base Number"].isin(selected_base_numbers)]
+    client_data = client_data[client_data["Asset Class"].isin(asset_classes)]
+    client_data["Asset Class"].replace({"Investment Cash & Short Term Investments": "Cash"}, inplace=True)
+    group_by_asset_class = client_data\
+    .groupby(['CCY','Asset Class'], as_index=False)\
+    .agg({'Nominal Amount (USD)':'sum'})
+    transformed_group_by_asset_class = \
+    group_by_asset_class.pivot(index='CCY', columns='Asset Class', values='Nominal Amount (USD)')\
+    .reset_index().fillna(0)
+
+    total_loans = transformed_group_by_asset_class.Loans.sum()
+    total_cash = transformed_group_by_asset_class.Cash.sum()
+    transformed_group_by_asset_class = transformed_group_by_asset_class.append({'CCY' : '<b>TOTAL</b>' , 'Cash' : f'<b>{total_cash}</b>', 'Loans' : f'<b>{total_loans}</b>'} , ignore_index=True)
+
+    fig = go.Figure(data=[go.Table(
+                header=dict(values=["Currency","Cash (USD)","Loans (USD)"],
+                            fill_color='paleturquoise',
+                            align='left'),
+                cells=dict(values=[transformed_group_by_asset_class.CCY, \
+                transformed_group_by_asset_class.Cash, 
+                transformed_group_by_asset_class.Loans],
+                        fill_color='lavender',
+                        align='left'))
+               ])
+
+    return fig
+
+@app.callback(
+    Output('cash_loans_barchart','figure'),
+    [Input('base_numbers_checklist', 'value')]
+) 
+def cash_loans_barchart(selected_base_numbers):
+    asset_classes = ['Investment Cash & Short Term Investments', 'Loans']
+    client_data = df[df["Base Number"].isin(selected_base_numbers)]
+    client_data = client_data[client_data["Asset Class"].isin(asset_classes)]
+    client_data["Asset Class"].replace({"Investment Cash & Short Term Investments": "Cash"}, inplace=True)
+    group_by_asset_class = client_data\
+    .groupby(['Asset Class','CCY'], as_index=False)\
+    .agg({'Nominal Amount (USD)':'sum'})
+
+    fig = px.bar(group_by_asset_class, x="CCY", y="Nominal Amount (USD)",
+            hover_data={"Nominal Amount (USD)":":.3f"},
+            text="Nominal Amount (USD)",color='Asset Class', barmode='group')
+    fig.update_layout(
+    title="Cash vs Liabilities",
+    xaxis_title="Currency",
+    yaxis_title="Total Nominal Amount (USD)",
+    legend_title="",
+    )
+    fig.update_traces(
+        textposition='outside',
+        texttemplate = "%{text:.2s}")
     return fig
 
 if __name__ == '__main__':
--- May/dash_pages/tempCodeRunnerFile.py
+++ May/dash_pages/tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
-asset_class_piechart
+asset_classes = ['Investment Cash & Short Term Investments', 'Loans']
+
+client_data = df[df["Asset Class"].isin(asset_classes)]
+#print(client_data)
+group_by_asset_class = client_data\
+.groupby(['Asset Class','CCY'], as_index=False)\
+.agg({'Nominal Amount (USD)':'sum'})
+print(group_by_asset_class)
