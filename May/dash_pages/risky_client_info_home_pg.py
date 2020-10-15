f0d8cce7b59a6bf3c1852e7a851808610bfc389b
May Myo Myint Myat <mmmmay.2017@sis.smu.edu.sg>
2020-10-15 14:48:37 +0800

    Cleaned up risky_client_info py file for home page

--- May/dash_pages/risky_client_info_home_pg.py
+++ May/dash_pages/risky_client_info_home_pg.py
@@ -18,6 +18,7 @@ import datetime as dt
 df = pd.read_csv('../Client.csv')
 risk_df = pd.read_csv('../RiskLevelsAllocation.csv')
 
+################# Data Processing for Risky Client Information #########################
 df['Position As of Date']= pd.to_datetime(df['Position As of Date']) 
 # df['Position As of Date'] = df['Position As of Date'].apply(lambda x: dt.datetime.strftime(x, '%Y-%d-%m'))
 # df['Position As of Date']= pd.to_datetime(df['Position As of Date']) 
@@ -29,19 +30,12 @@ latest_client_data["Asset Class"].replace({"Investment Cash & Short Term Investm
 risk_asset_classes = ["CASH", "FIXED INCOME", "EQUITIES"]
 risk_analysis_df = latest_client_data[latest_client_data["Asset Class"].isin(risk_asset_classes)]
 
-# total_nominal_amount = risk_analysis_df['Nominal Amount (USD)'].sum()
-# target_risk_level = risk_analysis_df["Target Risk Level"].iloc[0]
-
-# #print("Target Risk Level:",target_risk_level,type(target_risk_level))
-# target_risk_df = risk_df[risk_df['Level'] == target_risk_level]
-
 group_by_risk_asset_class = risk_analysis_df\
 .groupby(['Client Name','Asset Class'], as_index=True)\
 .agg({'Nominal Amount (USD)':'sum'})
 group_by_risk_asset_class["%"] = group_by_risk_asset_class.groupby(level=0).apply(lambda x:  100*x / x.sum())
 group_by_risk_asset_class = group_by_risk_asset_class.reset_index()
 #print(group_by_risk_asset_class)
-
 
 transformed_risk_analysis_df = \
     group_by_risk_asset_class.pivot(index='Client Name', columns='Asset Class', values='%')\
@@ -71,21 +65,29 @@ risk_analysis_df['Count'] = 1
 
 risk_levels = ["All Levels",1,2,3,4,5]
 
+################# End of Data Processing for Risky Client Information #########################
+
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div([
     dbc.Row([
+            # This is Overall Current Risk Profiles Breakdown Piechart
             dbc.Col([dcc.Graph(id='current_risk_piechart',
                 figure = px.pie(risk_analysis_df, values='Count', names='Current Risk Level',
                     title='Overall Current Risk Profiles Breakdown'
                 ).update_traces(textposition='inside', textinfo='percent+label').update_layout(legend_title="Current Risk Level")
             )], width={'size':3},),
+
+            # This is Target Status Breakdown Piechart
             dbc.Col([dcc.Graph(id='target_status_piechart',
                 figure = px.pie(risk_analysis_df, values='Count', names='Target Status',
                     title='Target Status Breakdown'
                 ).update_traces(textposition='inside', textinfo='percent+label')
             )], width={'size':3},),
+
+            # This is Filter + Table
             dbc.Col([
+                # This is Target Risk Level Filter for the Table below
                 dcc.Dropdown(
                         id='target_risk_filter',
                         options=[
@@ -93,19 +95,20 @@ app.layout = html.Div([
                         ],
                         placeholder="Filter by Target Risk Level",
                         # value=risk_levels[0],
-                        clearable=True),
+                        clearable=True),\
+                # This is Risk Target Status Table (connected to callback for "data" in DataTable) 
                 dash_table.DataTable(
-                id='risk_target_status_table',
-                columns = [{"name": i, "id": i} for i in risk_analysis_df[["Client Name","Current Risk Level","Target Risk Level"]].columns],
-                #data = risk_analysis_df[["Client Name","Current Risk Level","Target Risk Level"]].to_dict('records'),
-                # style_table={'border': 'thin lightgrey solid'},
-                # style_header={'backgroundColor':'lightgrey','fontWeight':'bold'},
-                style_cell={'textAlign':'center'}
+                    id='risk_target_status_table',
+                    columns = [{"name": i, "id": i} for i in risk_analysis_df[["Client Name","Current Risk Level","Target Risk Level"]].columns],
+                    style_table={'border': 'thin lightgrey solid'},
+                    style_header={'backgroundColor':'lightgrey','fontWeight':'bold'},
+                    style_cell={'textAlign':'center'}
                 )], 
                 width={'size':6})
             ]),
 ])
 
+# This callback is connected to Risk Target Status Table to return "data" in DataTable)
 @app.callback(
     Output('risk_target_status_table','data'),
     [Input('target_risk_filter', 'value')]
